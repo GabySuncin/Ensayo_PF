4cfbfbf6856a6efc2d010f3ff935e3fb4d1fad92
Javier <javiersalamanca101@gmail.com>
2024-06-25 22:18:33 -0600

    El pepe gano la guerra

--- intento1.cpp
+++ intento1.cpp
@@ -5,4 +5,5 @@ int main (void) {
     cout << "Estamos probando la rama develop" <<endl; 
     cout <<"La wea de la Gaby"<<endl;
     cout<<endl<<"OLA ke ASEN"<<endl;
+    cout<<endl<<"Elpepe"<<endl;
 }
