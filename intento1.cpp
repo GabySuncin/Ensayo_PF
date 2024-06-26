6a0a86757962ca97aaef162cd1223ef50053c112
LunaMartin00 <martinlunn1200@gmail.com>
2024-06-26 16:19:51 -0600

    Nomas estoy usando este repo porque no me contestaron y quiero practicar

--- intento1.cpp
+++ intento1.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <time.h>
+#include <stdlib.h>
+#include <fstream>
 using namespace std; 
 
-int main (void) {
-    cout << "Estamos probando la rama develop" <<endl; 
-    cout <<"La wea de la Gaby"<<endl;
-    cout<<endl<<"OLA ke ASEN"<<endl;
-    cout<<endl<<"Elpepe"<<endl;
+int main (void) 
+{
+    int puntaje;
+    puntaje = rand();
+    srand(time(NULL));
+    string us;
+    cout <<endl<<"ACA SE GUARDARAN LOS USUARIOS" <<endl; 
+    cout <<"INGRESE SU NOMBRE PORFAVOR: "<<endl;
+    cin>>us;
+    cout<<endl<<"Muchas gracias por jugar "<<us<<" :)"<<endl;
+    cout<<endl<<endl<<"Tu puntaje es: "<<puntaje<<endl;
+
+    
 }
