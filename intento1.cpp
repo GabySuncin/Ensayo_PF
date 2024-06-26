17688bd2e2fb3252cbcbc4a24aaed6e209cce9f0
LunaMartin00 <martinlunn1200@gmail.com>
2024-06-25 21:31:57 -0600

    No paso nada

--- intento1.cpp
+++ intento1.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main (void) {
     cout << "Estamos probando la rama develop" <<endl; 
-    cout << "Estoy escribiendo desde mi rama. Att: Gabs :D" <<endl; 
+    cout << "Estoy escribiendo desde mi rama. Att: Gabs :D" <<endl;
+
 }
