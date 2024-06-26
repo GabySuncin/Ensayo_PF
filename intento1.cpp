1b24098a4324b1eeefab58595afa6d9e3aa77f87
LunaMartin00 <martinlunn1200@gmail.com>
2024-06-25 21:59:03 -0600

    YA ESTOY HARTO

--- intento1.cpp
+++ intento1.cpp
@@ -4,5 +4,5 @@ using namespace std;
 int main (void) {
     cout << "Estamos probando la rama develop" <<endl; 
     cout <<"La wea de la Gaby"<<endl;
-    cout<<endl<<"HOLA ke ASEN"<<endl;
+    cout<<endl<<"OLA ke ASEN"<<endl;
 }
