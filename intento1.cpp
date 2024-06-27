5064ce20e9bfe961bad4ce9bc7dc1f1c8b34e23c
LunaMartin00 <martinlunn1200@gmail.com>
2024-06-26 19:24:48 -0600

    Sin miedo al exito

--- intento1.cpp
+++ intento1.cpp
@@ -6,17 +6,55 @@
 #include <fstream>
 using namespace std; 
 
+
 int main (void) 
 {
-    int puntaje;
-    puntaje = rand();
+    int puntaje, opcion;
     srand(time(NULL));
+    puntaje = rand();
     string us;
+    ofstream puntajeguar("puntajes.txt", ios::app);
+    if (!puntajeguar)
+    {
+        cout<<endl<<"EL ARCHIVO EN EL QUE DESEA GUARDAR LOS DATOS NO EXISTE"<<endl;
+    }  
     cout <<endl<<"ACA SE GUARDARAN LOS USUARIOS" <<endl; 
     cout <<"INGRESE SU NOMBRE PORFAVOR: "<<endl;
     cin>>us;
     cout<<endl<<"Muchas gracias por jugar "<<us<<" :)"<<endl;
     cout<<endl<<endl<<"Tu puntaje es: "<<puntaje<<endl;
+    puntajeguar<<"El puntaje de "<<us<<" es: "<<puntaje<<endl;
+    puntajeguar.close();
 
+
+    cout<<endl<<"Si deseas ver el historial de puntajes puedes seleccionar las siguientes opciones "<<endl;
+    cout<<"Presione [1] para ver: "<<"        Presione [0] Para salir: "<<endl;
+    cin>>opcion;
+    if (opcion == 1)
+    {
+        ifstream archivopuntos("puntajes.txt");
+        if (!archivopuntos.is_open())
+        {
+            cout<<endl<<"No se puede abrir el archivo puntajes.txt"<<endl;
+            
+        }
+        cout << "¡AQUI TIENES EL HISTORIAL HASTA AHORA!" << endl;
+        string linea;
+        while (getline(archivopuntos, linea)) 
+        {  
+            cout << linea << endl;
+        }
+
+        archivopuntos.close(); 
+        } 
+        else 
+        {
+            cout << endl << "¡FIN DE LA PARTIDA!" << endl;
+        }
+        
     
+    
+    
+
+    return 0;
 }
